19583f7b8e2499e16fd1ab60a2294d57e1cfd378
Byte Pair <bytepair@gmail.com>
2017-12-04 00:15:42 -0500

    Update day01.java

--- Day01/day01.java
+++ Day01/day01.java
@@ -2,10 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class day01 {
 
     public static void main(String[] args) {
-
         
         String result = "";
         int total = 0;
