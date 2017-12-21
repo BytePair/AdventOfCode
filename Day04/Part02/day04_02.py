72c3330a2515492cf6271ed2925277ad549855de
BytePair <bytepair@gmail.com>
2017-12-20 22:49:45 -0500

    Finish Python Day 04 Part 02

--- Day04/Part02/day04_02.py
+++ Day04/Part02/day04_02.py
@@ -1,25 +1,34 @@
-input_file = open("input.txt", "r")
+import itertools
 
-next_line = input_file.readline()
 
-valid_passphrase_count = 0
+def get_anagrams(word):
+    return ["".join(i) for i in itertools.permutations(word)]
 
-while (next_line):
 
-    passphrase = next_line.split()
-    valid_passphrase = True
-    used_words = []
-
-    for word in passphrase:
-        if word in used_words:
-            valid_passphrase = False
-        used_words.append(word)
-
-    if valid_passphrase:
-        valid_passphrase_count += 1
+if __name__ == '__main__':
+    
+    input_file = open("input.txt", "r")
 
     next_line = input_file.readline()
 
-input_file.close()
+    valid_passphrase_count = 0
 
-print("Answer:", valid_passphrase_count)
+    while (next_line):
+
+        passphrase = next_line.split()
+        valid_passphrase = True
+        used_words = []
+
+        for word in passphrase:
+            if word in used_words:
+                valid_passphrase = False
+            used_words.extend(get_anagrams(word))
+
+        if valid_passphrase:
+            valid_passphrase_count += 1
+
+        next_line = input_file.readline()
+
+    input_file.close()
+
+    print("Answer:", valid_passphrase_count)
