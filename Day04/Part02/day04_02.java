258e2f15854710cd8cbd9fa728ea996d4f5ed3c8
BytePair <bytepair@gmail.com>
2017-12-20 23:23:57 -0500

    Finish Java Day 04 Part 02

--- Day04/Part02/day04_02.java
+++ Day04/Part02/day04_02.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 
-public class day04_01 {
+public class day04_02 {
 
     public static void main(String[] args) {
 
@@ -20,8 +21,13 @@ public class day04_01 {
                 String[] splitLine = nextLine.split("\\W+");
                 HashSet<String> usedWords = new HashSet<String>();
                 for (String s: splitLine) {
+                    // if the string is already added, then the passphrase is no good
                     if (!usedWords.add(s)) {
                         validPassphrase = false;
+                    }
+                    // add all possible combinations for next loop
+                    for (String st: getCombinations(s)) {
+                        usedWords.add(st);
                     }
                 }
                 if (validPassphrase) {
@@ -38,6 +44,25 @@ public class day04_01 {
             e.printStackTrace();
         }
         
+    }
+
+    public static String[] getCombinations(String word) {
+        ArrayList<String> ans = new ArrayList<String>();
+        if (word.length() <= 1) {
+            ans.add(word);
+        }
+        else if (word.length() == 2) {
+            ans.add(word);
+            ans.add("" + word.charAt(1) + word.charAt(0));
+        }
+        else {
+            for (int i = 0; i < word.length(); i++) {
+                for (String s: getCombinations(word.substring(0, i) + word.substring(i+1))) {
+                    ans.add(word.charAt(i) + s);
+                }
+            }
+        }
+        return ans.toArray(new String[ans.size()]);
     }
 
 }
